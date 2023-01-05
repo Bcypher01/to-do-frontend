7afdc26c52a5a5c9ddc1c8a98e07a4571c00aa61
Bcypher01 <olumidebabayemi1@gmail.com>
2023-01-05 18:45:44 +0100

    work on form

--- pages/create-task.js
+++ pages/create-task.js
@@ -1,3 +1,4 @@
+import CreateTaskForm from "../components/CreateTaskForm";
 import FilterBtn from "../components/FilterBtn";
 
 const createTask = () => {
@@ -8,50 +9,7 @@ const createTask = () => {
         <FilterBtn />
       </div>
       <div className="my-5 px-8">
-        <form action="" method="post" className="w-full">
-          <div className="relative z-0 mb-6 w-full group">
-            <input
-              type="name"
-              name="floating_name"
-              id="floating_name"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required
-            />
-            <label
-              htmlFor="floating_email"
-              className="peer-focus:font-medium absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Name
-            </label>
-          </div>
-
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-state"
-          >
-            Status
-          </label>
-          <div className="relative">
-            <select
-              className="block appearance-none w-full bg-gray-100 border border-gray-100 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-state"
-            >
-              <option>Done</option>
-              <option>In Progress</option>
-              <option>Pending</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
-            </div>
-          </div>
-        </form>
+        <CreateTaskForm />
       </div>
     </div>
   );
